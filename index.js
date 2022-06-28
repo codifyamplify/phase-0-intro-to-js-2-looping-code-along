d59798a5e44533b29685965ef20433402cfa6a7e
David Whitney <codifyamplify@gmail.com>
2022-06-28 14:26:34 -0400

    commit tree 53449c936549f258a7931138f7248e6e3704161d

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solutions in this file
+// // Code your solutions in this file
+function writeCards(namesArray, eventName){
+    let resultingCards = []
+    for(let i=0; i < namesArray.length; i++){
+        resultingCards.push(`Thank you, ${namesArray[i]}, for the wonderful ${eventName} gift!`)
+    }
+    return resultingCards
+}
+
+function countDown(initialInteger){
+    let counter = initialInteger
+    while (counter > 0) {
+
+        console.log(counter)
+        counter -= 1
+    }
+    console.log(counter)
+}
+
+countDown(10)
